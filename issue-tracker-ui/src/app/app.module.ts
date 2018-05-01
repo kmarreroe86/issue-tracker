5a22dcefb1b68fb8fee2947ad855c3c1686061b3
Karel Marrero <kmarreroe86@gmail.com>
2018-04-30 19:55:17 -0500

    Init ui project

--- issue-tracker-ui/src/app/app.module.ts
+++ issue-tracker-ui/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+
+const appRoutes: Routes = [
+  { path: '', component: LoginComponent },
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
